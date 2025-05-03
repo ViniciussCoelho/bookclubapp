572c294cbbc400350bdcda0006a1fb1fea32e8e8
Vinicius Coelho <vinicius2001@gmail.com>
2025-05-03 18:14:31 +0000

    fix: default field

--- db/migrate/20250503180330_create_club.rb
+++ db/migrate/20250503180330_create_club.rb
@@ -1,9 +1,9 @@
 class CreateClub < ActiveRecord::Migration[7.2]
   def change
     create_table :clubs do |t|
-      t.string :name
-      t.string :descripion
-      t.jsonb :chat_messages
+      t.string :name, null: false
+      t.string :description, null: false
+      t.jsonb :chat_messages, default: []
 
       t.timestamps
     end
